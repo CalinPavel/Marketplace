d15655df17da5890b79cdf2572edfcd85d3f5a81
cali <cali@kali.home>
2023-04-22 11:03:37 +0300

    unit testing

--- 1-marketplace/skel/tema/marketplace.py
+++ 1-marketplace/skel/tema/marketplace.py
@@ -5,14 +5,18 @@ Computer Systems Architecture Course
 Assignment 1
 March 2021
 """
+import unittest
+from tema.product import Product, Tea
+from tema.producer import Producer
 
+import threading
 
 import logging
 from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger('logger')
 logger.setLevel(logging.INFO)
-handler = RotatingFileHandler('marketplace.log', maxBytes=4000, backupCount=10)
+handler = RotatingFileHandler('marketplace.log', maxBytes=5000, backupCount=10)
 logger.addHandler(handler)
 
 from threading import Lock, currentThread
@@ -50,11 +54,12 @@ class Marketplace:
         """
         Returns an id for the producer that calls this.
         """
+        logger.info('register_producer')
 
         with self.return_producer_id:
             self.producer_queue.append(0)
-            logger.info('register_producer')
             return len(self.producer_queue)-1
+
 
     def publish(self, producer_id, product):
         """
@@ -81,21 +86,19 @@ class Marketplace:
 
         return True
 
-        pass
-
     def new_cart(self):
         """
         Creates a new cart for the consumer
 
         :returns an int representing the cart_id
         """
+        logger.info('new_cart')
 
         with self.create_new_cart:
             self.cart_count += 1
             cart_id = self.cart_count
 
         self.cart_dic[cart_id] = []
-        logger.info('new_cart')
         return cart_id
 
 
@@ -111,6 +114,7 @@ class Marketplace:
 
         :returns True or False. If the caller receives False, it should wait and then try again
         """
+        logger.info('add_to_cart')
 
         with self.modify_cart:            
             check=False
@@ -119,17 +123,14 @@ class Marketplace:
                 if self.products[i] == product:
                     check=True
             
-            if check == False:
+            if check is False:
                 return check
 
             self.producer_queue[self.producers_dic[product]] -= 1
             self.products.remove(product)
 
         self.cart_dic[cart_id].append(product)
-        logger.info('add_to_cart')
         return True
-
-
 
     def remove_from_cart(self, cart_id, product):
         """
@@ -141,12 +142,18 @@ class Marketplace:
         :type product: Product
         :param product: the product to remove from cart
         """
-        self.cart_dic[cart_id].remove(product)
-        self.producer_queue.append(product)
-
-        with self.modify_cart:
-            self.producer_queue[self.producers_dic[product]] += 1
+ 
         logger.info('remove_from_cart')
+
+        try:
+            with self.modify_cart:
+                self.cart_dic[cart_id].remove(product)
+                self.producer_queue.append(product)
+                self.producer_queue[self.producers_dic[product]] += 1
+                return True
+        except:
+                return False
+                    
         
 
     def place_order(self, cart_id):
@@ -157,10 +164,65 @@ class Marketplace:
         :param cart_id: id cart
         """
 
+        logger.info('place_order')
         for prod in self.cart_dic.pop(cart_id, None):
             with self.print_mutex:
-                print("{} bought {}".format(currentThread().getName(), prod))
-
-        logger.info('place_order')
+                print("{} bought {}".format(threading.current_thread().getName(), prod))
 
         return self.cart_dic.pop(cart_id, None)
+
+class TestMarketplace(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = Marketplace(3)
+
+        self.pro_1 = Product('Wild Cherry' , '5')
+        self.tea_1 = Tea(self.pro_1,'Black' , '5')
+        self.pro_2 = Product('Cactus fig' , '3')
+        self.tea_2 = Tea(self.pro_2,'Green' , '3')
+        self.list = []
+        self.list.append(self.tea_1)
+        self.list.append(self.tea_2)
+
+    def test_register_producer(self):
+        self.assertEqual(self.obj.register_producer() , 0)
+        self.assertEqual(self.obj.register_producer() , 1)
+        self.assertEqual(self.obj.register_producer() , 2)
+    
+
+    def test_publish(self):
+        self.obj.register_producer()
+        self.assertEqual(self.obj.publish(0,self.pro_1),True)
+
+    def test_new_cart(self):
+        self.assertEqual(self.obj.new_cart(),1)
+        self.assertEqual(self.obj.new_cart(),2)
+
+    def test_add_to_cart(self):
+        self.obj.register_producer()
+        self.obj.publish(0,self.pro_1)
+        self.assertEqual(self.obj.new_cart(),1)
+        self.assertEqual(self.obj.new_cart(),2)
+        self.assertEqual(self.obj.add_to_cart(1 ,self.pro_1),True)
+        self.assertEqual(self.obj.add_to_cart(1 ,self.pro_2),False)
+
+    def test_remove_from_cart(self):
+        self.obj.register_producer()
+        self.obj.publish(0,self.pro_1)
+        self.assertEqual(self.obj.new_cart(),1)
+        self.assertEqual(self.obj.new_cart(),2)
+        self.assertEqual(self.obj.add_to_cart(1 ,self.pro_1),True)
+        self.assertEqual(self.obj.remove_from_cart(1 ,self.pro_1),True)
+        self.assertEqual(self.obj.remove_from_cart(5 ,self.pro_1),False)
+
+
+    def test_place_order(self):
+        self.obj.register_producer()
+        self.obj.publish(0,self.pro_1)
+        self.assertEqual(self.obj.new_cart(),1)
+        self.assertEqual(self.obj.add_to_cart(1 ,self.pro_1),True)
+        self.assertEqual(self.obj.place_order(1),None)
+
+
+if __name__ == '__main__':
+    unittest.main()
